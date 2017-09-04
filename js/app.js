df44ab03dfcc8c2077d94ca5b5cbf4aa5d4fa513
Katie Allyn <kaallyn@gmail.com>
2017-09-04 09:10:52 -0500

    grid system and basic styling in place

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// $(document).ready( function() {
+//     $('#datePicker').val(new Date().toDateInputValue());
+// });​
